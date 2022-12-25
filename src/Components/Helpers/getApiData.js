e2ddeea1906dcafae86e120ca941bc8dae5cdcc4
Nana <nanamordov@gmail.com>
2022-12-25 21:23:17 +0200

    rpelace fetch with axios

--- src/Components/Helpers/getApiData.js
+++ src/Components/Helpers/getApiData.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 // import { API_URL_RICK_AND_MORTY, API_URL_BREAKING_BAD } from "../../config";
 import { API_URL_RICK_AND_MORTY } from "../../config";
@@ -11,11 +12,11 @@ const useGetApiData = () => {
 
   const fetchCharacters = async () => {
     try {
-      const response = await fetch(`${API_URL_RICK_AND_MORTY}character`);
-      //const response = await fetch(`https://cors-anywhere.herokuapp.com/${API_URL_BREAKING_BAD}characters`);
-      const charactersData = await response.json();
-      const ListOfCharacters = charactersData.results.slice(0, 15);
-      setCharacters(ListOfCharacters);
+      axios.get(`${API_URL_RICK_AND_MORTY}/character`).then((response) => {
+        const charactersData = response.data;
+        const ListOfCharacters = charactersData.results.slice(0, 15);
+        setCharacters(ListOfCharacters);
+      });
     } catch (error) {
       console.log(error);
     }
